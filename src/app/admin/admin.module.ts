7d74de5f00ea7954395fb8afecba36e65ff766e6
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-10-25 13:06:46 +0100

    added card and slide toggle to module #BM-35

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
@@ -7,7 +9,12 @@ import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [ProductsListComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MatCardModule,
+    MatSlideToggleModule,
+  ],
   exports: [AdminRoutingModule],
   providers: [ProductService],
 })
