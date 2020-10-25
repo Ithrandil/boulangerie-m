880d846392e5b65114f2375b83224e9058e41a0f
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-10-25 18:25:38 +0100

    added prod env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: true,
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyB5A2l7x2ajzt_5_Pw4RYfidnKva1VD39c',
+    authDomain: 'boulangerie-m.firebaseapp.com',
+    databaseURL: 'https://boulangerie-m.firebaseio.com',
+    projectId: 'boulangerie-m',
+    storageBucket: 'boulangerie-m.appspot.com',
+    messagingSenderId: '647985520950',
+    appId: '1:647985520950:web:f6632252340eee0285d397',
+    measurementId: 'G-QVHB573NPH',
+  },
 };
