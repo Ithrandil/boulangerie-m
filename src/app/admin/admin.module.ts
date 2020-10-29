9f629a4ca34af07b7214023ae996e3b3d326845c
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-10-30 00:52:13 +0100

    restyling product cards and accordion for description

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AdminRoutingModule } from './admin-routing.module';
@@ -14,6 +15,7 @@ import { ProductService } from './services/product.service';
     AdminRoutingModule,
     MatCardModule,
     MatSlideToggleModule,
+    MatExpansionModule,
   ],
   exports: [AdminRoutingModule],
   providers: [ProductService],
