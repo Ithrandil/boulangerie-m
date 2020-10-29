50ab4f34c9ca81a7b75a96c3633beb275dc19eb5
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-10-29 19:02:17 +0100

    simplified order model #BM-25

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,19 +1,15 @@
 import { ProductUnit } from './product';
 
 export interface Order {
-  user: User;
-  order: [{ product: string; quantity: number; unit: ProductUnit }];
-  deliveryTime: Date;
-  orderTime: Date;
-  totalPrice: number;
-}
-
-export interface User {
   name: string;
   phone: number;
   address: Address;
   hasDifferentDeliveryAddress: boolean;
   deliveryAddress: Address;
+  order: [{ product: string; quantity: number; unit: ProductUnit }];
+  deliveryTime: Date;
+  orderTime: Date;
+  totalPrice: number;
 }
 
 export interface Address {
