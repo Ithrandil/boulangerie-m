b9609c32101bfeee54a54d658abcddcce5c1a9ef
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-11-01 12:21:48 +0100

    first header implementation #BM-24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -29,6 +30,7 @@ registerLocaleData(localeFr, 'fr-FR', localeFrExtra);
     OrderModule,
     FlexLayoutModule,
     AuthModule,
+    MatToolbarModule,
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }, AngularFireAuthGuard],
   bootstrap: [AppComponent],
