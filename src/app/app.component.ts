5bfdc1f8f4fc5ec55b4edbae491fedbde73fad37
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-11-07 16:14:47 +0100

    dynamic header menu admin and logout footer #BM-24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '@app/auth/services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public router: Router) {}
+  public isConnected!: firebase.User | null;
+  public activatedRoute = '';
+  constructor(public router: Router, public authService: AuthService) {
+    this.authService
+      .isUserAuthenticated()
+      .pipe(
+        tap((user) => {
+          this.isConnected = user;
+        })
+      )
+      .subscribe();
+    console.log(this.router.routerState.snapshot.url);
+  }
+  public logoutAdminSpace(): void {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
+  public onActivate(): void {
+    this.activatedRoute = this.router.routerState.snapshot.url;
+  }
 }
