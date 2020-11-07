bef4cea479fc755056acf4272643e7e8e9e4fdff
Alexandre Guérin <alexandre.guerin17@gmail.com>
2020-11-07 18:56:43 +0100

    creation of mentions legales page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { MentionsLegalesComponent } from './core/pages/mentions-legales/mentions-legales.component';
 import { OrderFormComponent } from './order/pages/order-form/order-form.component';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['connexion']);
@@ -15,6 +16,7 @@ const redirectAlreadyLoggedIn = () => redirectLoggedInTo(['admin']);
 const routes: Routes = [
   { path: '', redirectTo: 'commande', pathMatch: 'full' },
   { path: 'commande', component: OrderFormComponent },
+  { path: 'mentions-legales', component: MentionsLegalesComponent },
   {
     path: 'connexion',
     component: LoginPageComponent,
@@ -27,7 +29,7 @@ const routes: Routes = [
     ...canActivate(redirectUnauthorizedToLogin),
   },
   // { path: '404', component: PageNotFoundPage },
-  // { path: '**', redirectTo: '404' },
+  { path: '**', redirectTo: 'commande' },
 ];
 
 @NgModule({
