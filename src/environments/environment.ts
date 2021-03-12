011c2c3e4b38ecbee18c30b22e69bc7521ad6d58
Alexandre Guérin <alexandre.guerin17@gmail.com>
2021-03-12 11:05:03 +0100

    transfer to client account

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,13 @@ export const environment = {
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebaseConfig: {
-    apiKey: 'AIzaSyB5A2l7x2ajzt_5_Pw4RYfidnKva1VD39c',
-    authDomain: 'boulangerie-m.firebaseapp.com',
-    databaseURL: 'https://boulangerie-m.firebaseio.com',
-    projectId: 'boulangerie-m',
-    storageBucket: 'boulangerie-m.appspot.com',
-    messagingSenderId: '647985520950',
-    appId: '1:647985520950:web:f6632252340eee0285d397',
-    measurementId: 'G-QVHB573NPH',
+    apiKey: 'AIzaSyAdqYq28vpm_FbbmvbxFju8m0kOyKrfoaU',
+    authDomain: 'boulangerie-m17.firebaseapp.com',
+    projectId: 'boulangerie-m17',
+    storageBucket: 'boulangerie-m17.appspot.com',
+    messagingSenderId: '615948873412',
+    appId: '1:615948873412:web:996ee9e9433f1db3016391',
+    measurementId: 'G-SNJSWSZG0K',
   },
 };
 
