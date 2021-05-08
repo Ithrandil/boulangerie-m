a9b136d4555c2ed965b9082afdd9fc496623862b
Ithrandil <alexandre.guerin17@gmail.com>
2021-05-08 10:27:32 +0200

    fixed negative order and 0 item in summary

--- src/app/order/services/order-summary.pipe.ts
+++ src/app/order/services/order-summary.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'summaryPipe' })
 export class OrderSummaryPipe implements PipeTransform {
   transform(arr: [string, number][]): string[] {
-    const tmp = arr.filter((el) => el[1] !== null);
+    const tmp = arr.filter((el) => el[1] !== null && el[1] > 0);
     const res: string[] = [];
     tmp.forEach((el) => res.push(`- ${el[0]} : ${el[1]}`));
     return res;
