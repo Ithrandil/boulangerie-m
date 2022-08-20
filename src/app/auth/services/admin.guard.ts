b4291e23395d42f38ced0877c52b0dc0baee7c52
Alexandre Guérin <alexandre.guerin17@gmail.com>
2022-08-20 16:14:32 +0200

    fix: bonne gestion admin guard

--- src/app/auth/services/admin.guard.ts
+++ src/app/auth/services/admin.guard.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { take } from 'rxjs';
+import { CanActivate } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-    constructor(
-        private authService: AuthService,
-        private router: Router) { }
-    canActivate(): boolean | Promise<boolean> {
-        let isAuthenticated = false;
-        this.authService.isUserAdmin().pipe(take(1)).subscribe({ next: (isAuth) => isAuthenticated = isAuth, error: () => isAuthenticated = false });
-        if (!isAuthenticated) {
-            this.router.navigate(['/']);
-        }
-        return isAuthenticated;
+    constructor(private authService: AuthService) { }
+
+    canActivate(): Observable<boolean> {
+        return this.authService.isUserAdmin().pipe(map(isAuth => isAuth));
     }
 }
